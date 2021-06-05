30d8ec25832970a6255ab034640740313279d16d
BriagasD98 <briagasdavid@yahoo.com>
2021-06-05 01:59:18 -0500

    progress

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+// -- Questions & Answers --
+
+// Inside which HTML element do we put the JavaScript?
+// <javascript>
+// <js>
+// <espresso>
+// <script>
+
+// How do you write "Drink Some Coffee!" in an alert box?
+// alert("Drink some Coffee!");
+// popUp("Drink some Coffee!");
+// box("Drink some Coffee!");
+// alertMe("Drink some Coffee!");
+
+// How do you write a comment in JavaScript?
+// // This is a comment
+// <!-- This is a comment --!>
+// This is a comment
+// ("This is a comment")
+
+// Which is the correct way to write an array in JavaScript?
+// var = {Mazda, Honda, Toyota, Nissan}
+// var = ["Mazda", "Honda", "Toyota", "Nissan"]
+// var = <"Mazda", "Honda", "Toyota", "Nissan">
+// var = |Mazda, Honda, Toyota, Nissan|
+
+// How do you declare a JavaScript variable?
+// var carName
+// variable = carName
+// var = carName
+// var == carName
+
+// How do you write an "if" statement in JavaScript?
+// if i = 9
+// if (i == 9)
+// if i++
+// if 7 8 9
+
+// How do you find the number with the highest value of x and y?
+// maxValue(x,y)
+// max(x,y)
+// Math.max(x,y)
+// valueMax(x,y)
+
+// How can you detect the client's browser name?
+// navigator.appName
+// clientApp.Name
+// clientBrowser.name
+// nav.app.name
+
+// What will the following code return: Boolean(7 < 9)
+// NaN
+// false
+// true
+// 7 Ate 9
